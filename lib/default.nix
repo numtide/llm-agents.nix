e56b78c2aae79af432ba290108349521917e5638
Anton Vasiljev <antono.vasiljev@gmail.com>
2026-05-10 09:55:45 +0300

    lib: add antono to maintainers

--- lib/default.nix
+++ lib/default.nix
@@ -117,6 +117,11 @@ inputs.nixpkgs.lib.extend (
         githubId = 5122800;
         name = "pikdum";
       };
+      antono = {
+        github = "antono";
+        githubId = 7622;
+        name = "Antono";
+      };
     };
   }
 )
